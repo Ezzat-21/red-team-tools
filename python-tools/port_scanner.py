26eae01cf5778ce5be977b663acbb6a2185d91c7
Ahmed Ezzat <ahmedezza9111@gmail.com>
2026-08-12 16:36:18 -0400

    L20 SUID enumeration done, N20 FTP credential capture in Wireshark, P20 fix  port_scanner.py with sys.argv

--- python-tools/port_scanner.py
+++ python-tools/port_scanner.py
@@ -1,19 +1,18 @@
 import socket
+import sys
 
-IP = '192.168.56.101'
+IP = '192.168.56.104'
+start = int(sys.argv[1])
+end = int(sys.argv[2])
 
+print(f"Scanning {IP} ports {start}-{end}...")
 
-print("Scanning target...")
-
-for port in range(1,1025):
-
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.settimeout(1)
-
-    result = s.connect_ex((IP,port))
-
+for port in range(start, end):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.1)
+    result = s.connect_ex((IP, port))
     if result == 0:
         print(f"port {port} is open")
-    s.close()   
+    s.close()
 
 print("Scan completed.")

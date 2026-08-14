6f9fd5b32d09b1b88f2e1eeda5d3ff4cdba22d5f
Ahmed Ezzat <ahmedezza9111@gmail.com>
2026-08-14 13:39:25 -0400

    L21 SUID escalation attempt, N21 tcpdump SSH capture, P21 added argparse to port_scanner.py

--- python-tools/port_scanner.py
+++ python-tools/port_scanner.py
@@ -1,16 +1,46 @@
 import socket
-import sys
+import argparse
 
-IP = '192.168.56.104'
-start = int(sys.argv[1])
-end = int(sys.argv[2])
 
-print(f"Scanning {IP} ports {start}-{end}...")
+parser = argparse.ArgumentParser(description='Port Scanner')
 
-for port in range(start, end):
+parser.add_argument(
+    "-t",
+    "--target",
+    required=True,
+    help="Target IP address"
+)
+parser.add_argument(
+    "-s",
+    "--start",
+    type=int,
+    default=1,
+    help="Scan Starting Position"
+)
+parser.add_argument(
+    "-e",
+    "--end",
+    type=int,
+    default=1025,
+    help="Scan Ending Position"
+)
+parser.add_argument(
+    "-T",
+    "--timeout",
+    type=float,
+    default=0.1,
+    help="Timeout Period"
+)
+args = parser.parse_args()
+
+
+
+print(f"Scanning {args.target} ports {args.start}-{args.end}...")
+
+for port in range(args.start, args.end + 1 ):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.1)
-    result = s.connect_ex((IP, port))
+    s.settimeout(args.timeout)
+    result = s.connect_ex((args.target, port))
     if result == 0:
         print(f"port {port} is open")
     s.close()
